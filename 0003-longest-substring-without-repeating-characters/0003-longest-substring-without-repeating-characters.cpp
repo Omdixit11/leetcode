6d18c4a53e2c2cd7b8b54de2f5a17e008b60eae1
Om Dixit <dixitom576@gmail.com>
2026-09-18 10:14:54 +0530

    Time: 302 ms (21.76%), Space: 81.4 MB (20.49%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> st;
+
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+
+        while (right < s.length()) {
+
+            // If character is already present
+            while (st.find(s[right]) != st.end()) {
+                st.erase(s[left]);
+                left++;
+            }
+
+            // Add current character
+            st.insert(s[right]);
+
+            // Calculate length
+            maxLen = max(maxLen, right - left + 1);
+
+            right++;
+        }
+
+        return maxLen;
+    }
+};
